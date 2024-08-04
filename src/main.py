6a62b57aaf443df4730a3a035b3f4898c449dff6
zecheng <zechengwang724@gmail.com>
2024-08-04 12:19:52 +0800

    Feat: raw data處理前的篩選功能
    因應需求：
    1. 每筆資料的狀態不同，不一定需要所有資料都進行處理
    
    調整項目：
    1. parser.py。新增 should_skip 函數，對資料的指定位置進行條件判斷。若需要跳過，則不進行後續的編碼、格式轉換處理

--- src/main.py
+++ src/main.py
@@ -7,13 +7,27 @@ from parser import parse_file
 data_file = 'data/raw/f6_01000001_01001000_TP03.new'
 output_file = 'data/processed/parsed_data.json'
 
+
+skip_conditions = [
+	{
+		'position': 22,
+		'value': (0b10000000, 0b10000000),  # 檢查 reveal_flags 的 Bit 7 是否為 1
+		'mode': 'include'
+	},
+	{
+		'position': 24,
+		'value': (0b10000000, 0b10000000),  # 檢查 status_flags 的 Bit 7 是否為 1
+		'mode': 'include'
+	}
+]
+
 def main():
 	# 解析原始資料
-	data = parse_file(data_file)
+	data = parse_file(data_file, skip_conditions)
 
 	# 將轉換後資料保存成JSON
 	with open(output_file, 'w', encoding='utf-8') as json_file:
-	    json.dump(data, json_file, ensure_ascii=False, indent=4)
+		json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 	print(f'Data has been successfully written to {output_file}')
 
